d332160e709bf80e94151da0796e64210ea8072f
brocahontaz <johan.fjallbark@gmail.com>
2024-01-27 16:48:42 +0100

    outputs

--- terraform/_outputs.tf
+++ terraform/_outputs.tf
@@ -1,4 +1,7 @@
-output "node_user_pwd" {
-  value     = module.k3s_cluster.node_user_pwd
+output "k3s_info" {
   sensitive = true
+  value = {
+    node_user_pwd = module.k3s_cluster.node_user_pwd
+    nodes = module.k3s_cluster.nodes
+  }
 }
